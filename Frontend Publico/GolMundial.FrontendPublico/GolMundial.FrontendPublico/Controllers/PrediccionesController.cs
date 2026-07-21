121fb61f2f780413f01beee8562787d075da2c5e
estebanhuera06 <estebanhuera06@gmail.com>
2026-07-20 22:44:43 -0500

    agregar el controlador y vistas de predicciones

---
+++ Frontend Publico/GolMundial.FrontendPublico/GolMundial.FrontendPublico/Controllers/PrediccionesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GolMundial.FrontendPublico.Controllers
+{
+    [Authorize]
+    public class PrediccionesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Nueva(int partidoId)
+        {
+            ViewData["PartidoId"] = partidoId;
+            return View();
+        }
+    }
+}
