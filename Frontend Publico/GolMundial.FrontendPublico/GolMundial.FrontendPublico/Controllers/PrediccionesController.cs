0a03f767397ade090e794930701040e58addd6f4
estebanhuera06 <estebanhuera06@gmail.com>
2026-07-21 21:27:31 -0500

    creacion de la vista para predicciones y imagen de fondo de todas las vistas

--- Frontend Publico/GolMundial.FrontendPublico/GolMundial.FrontendPublico/Controllers/PrediccionesController.cs
+++ Frontend Publico/GolMundial.FrontendPublico/GolMundial.FrontendPublico/Controllers/PrediccionesController.cs
@@ -1,6 +1,8 @@
-﻿using GolMundial.FrontendPublico.Services;
+﻿using GolMundial.FrontendPublico.Models;
+using GolMundial.FrontendPublico.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace GolMundial.FrontendPublico.Controllers
 {
@@ -8,11 +10,17 @@ namespace GolMundial.FrontendPublico.Controllers
     public class PrediccionesController : Controller
     {
         private readonly IPartidoService _partidoService;
+        private readonly IPrediccionService _prediccionService;
 
-        public PrediccionesController(IPartidoService partidoService)
+        public PrediccionesController(
+            IPartidoService partidoService, 
+            IPrediccionService prediccionService)
         {
             _partidoService = partidoService;
+            _prediccionService = prediccionService;
         }
+
+        private int UsuarioId => int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
 
         [HttpGet]
         public async Task<IActionResult> Nueva(int partidoId)
@@ -25,7 +33,52 @@ namespace GolMundial.FrontendPublico.Controllers
             if (partido.Estado != "PROGRAMADO")
                 return RedirectToAction("Index", "Home");
 
-            return View(partido);
+            if (await _prediccionService.ObtenerDelPartidoAsync(UsuarioId, partidoId) is not null)
+            {
+                TempData["Aviso"] = "Ya hiciste una predicción para ese partido.";
+                return RedirectToAction(nameof(Mias));
+            }
+
+            return View(new PrediccionFormViewModel
+            {
+                Partido = partido,
+                Saldo = await _prediccionService.ObtenerSaldoAsync(UsuarioId),
+                Input = new PrediccionInput { PartidoId = partidoId }
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Nueva(PrediccionFormViewModel vm)
+        {
+            var partido = await _partidoService.ObtenerPorIdAsync(vm.Input.PartidoId);
+
+            if (partido is null)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                var resultado = await _prediccionService.CrearAsync(UsuarioId, vm.Input);
+
+                if (resultado.Exito)
+                {
+                    TempData["Aviso"] = "¡Predicción registrada!";
+                    return RedirectToAction(nameof(Mias));
+                }
+
+                ModelState.AddModelError(string.Empty, resultado.Error!);
+            }
+
+            vm.Partido = partido;
+            vm.Saldo = await _prediccionService.ObtenerSaldoAsync(UsuarioId);
+            return View(vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Mias()
+        {
+            ViewData["Saldo"] = await _prediccionService.ObtenerSaldoAsync(UsuarioId);
+            return View(await _prediccionService.ObtenerPorUsuarioAsync(UsuarioId));
         }
     }
 }
---
+++ Frontend Publico/GolMundial.FrontendPublico/GolMundial.FrontendPublico/Models/PrediccionFormViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GolMundial.FrontendPublico.Models
+{
+    public class PrediccionFormViewModel
+    {
+        public Partido Partido { get; set; } = null!;
+        public int Saldo { get; set; }
+        public PrediccionInput Input { get; set; } = new();
+    }
+}
